06333e427099e518f418b8897a288eb6c70e38fa
Erick <kire.erickoz@gmail.com>
2024-11-13 16:25:12 -0600

    actualizacion

--- Cursos Java/Matriz/src/Ejercicios.java
+++ Cursos Java/Matriz/src/Ejercicios.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicios {
@@ -5,16 +6,17 @@ public class Ejercicios {
 
 //      ejercicio1(); //Ejercicio1
 //        ejercicio2();
-        ejercicio3();
+        ejercicio5();
 
 
     }
-    //TODO: EJERCICIO 1 CON METODOS
+
+//    TODO: EJERCICIO 1 CON METODOS
     public static void ejercicio1(){
-        //PROBLEMA
-        //Crea un array o una matriz de diez posiciones (tamaño diez) de números con valores pedidos por teclado.
-        //Muestra por consola el índice de la matriz y el valor que le corresponde.
-        //Crea dos métodos uno para rellenar valores y otro para mostrar.
+//        PROBLEMA
+//        Crea un array o una matriz de diez posiciones (tamaño diez) de números con valores pedidos por teclado.
+//        Muestra por consola el índice de la matriz y el valor que le corresponde.
+//        Crea dos métodos uno para rellenar valores y otro para mostrar.
 
         int[] matriz10 = new int[10];
 
@@ -38,10 +40,10 @@ public class Ejercicios {
         }
     }
 
-    //TODO:EJERCICIO 2
+//   TODO:EJERCICIO 2
     public static void ejercicio2(){
-        //    Crea un array de números de 100 posiciones, es decir, tamaño 100 que contendrá los números del uno al 100.
-        //    Obtener la suma de todos los números del array y obtener también la media.
+//    Crea un array de números de 100 posiciones, es decir, tamaño 100 que contendrá los números del uno al 100.
+//    Obtener la suma de todos los números del array y obtener también la media.
 
         int[] matriz100 = new int[100];
 
@@ -61,7 +63,7 @@ public class Ejercicios {
 
     }
 
-    //TODO: EJERCICIO 3
+//    TODO: EJERCICIO 3
 //    A partir de un array de números de cinco posiciones con los siguientes valores 12345.
 //    Guardar los valores de este array en otro array distinto pero con los valores invertidos.
 //    Es decir que el segundo array deberá tener los valores 54321.
@@ -79,6 +81,38 @@ public class Ejercicios {
         System.out.println("\nMatriz invertida:");
         for (int i : matriz5Invertido) {
             System.out.print(i);
+        }
+    }
+
+//    TODO: EJERCICIO 4
+//    Rellenar un array de números de tipo entero (int) utilizando el método fill
+//    de la clase Arrays y Muéstralos por consola.
+    private static void ejercicio4(){
+
+        int[] matriz = new int[5];
+
+//        Arrays.fill(matriz,5);
+        Arrays.fill(matriz,2,4,3);
+
+        for (int i : matriz){
+            System.out.println(i);
+        }
+    }
+
+//    TODO: EJERCICIO 5
+//    Crear dos arrays, Los dos tienen que ser de tamaño diez.
+//    Rellenar uno con números. Puedes rellenarlo a mano utilizando el método Fill y copiarlo al otro utilizando copy of the arrays.
+//    Otro método finalmente mostrar los números copiados en el segundo array.
+    private static void ejercicio5(){
+        int[] matriz = new int[10];
+        int[] matrizCopy = new int[10];
+
+        Arrays.fill(matriz, 4);
+
+        matrizCopy = Arrays.copyOfRange(matriz, 1,6);
+
+        for (int i: matrizCopy) {
+            System.out.println(i);
         }
     }
 
