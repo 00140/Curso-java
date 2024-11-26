5430bf660e7a00eaa6dee16e5ccee5e79dce33e6
erick <kire.erickoz@gmail.com>
2024-11-25 18:03:26 -0600

    interfaz

--- Cursos Java/EvaSockets/src/JarServidor/Ventana.java
+++ Cursos Java/EvaSockets/src/JarServidor/Ventana.java
@@ -19,133 +19,77 @@ public class Ventana extends JFrame {
 
         JPanel panel = new JPanel();
         panel.setBackground(Color.darkGray); //cambiar fondo
-        panel.setLayout(new GridLayout(8,2));
+        panel.setLayout(new GridBagLayout());
 
         GridBagConstraints constraints = new GridBagConstraints();
 
-        JLabel etiquetaTitulo = new JLabel("SERVIDOR");
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
+        JLabel etiquetaTitulo = new JLabel("SERVIDOR",SwingConstants.CENTER);
+        metodoConstraints(constraints, 0,0,10,1,1,1);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(etiquetaTitulo,constraints);
 
-        panel.add(etiquetaTitulo, constraints);
+        constraints = new GridBagConstraints();
+        JLabel etiquetaIP = new JLabel("Ip");
+        metodoConstraints(constraints, 0,1,3,1,1,0);
+        panel.add(etiquetaIP,constraints);
 
-        JTextArea cajaTexto = new JTextArea("Area texto");
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        panel.add(cajaTexto, constraints);
+        JLabel etiquetaPuerto = new JLabel("puerto");
+        metodoConstraints(constraints, 2,1,3,1,1,1);
+        panel.add(etiquetaPuerto,constraints);
 
-        // JButton en la fila 0, columna 2 y que ocupa 1 celda de ancho por
-        // una de alto.
-        JButton boton1 = new JButton ("Boton 1");
-        constraints.gridx = 2;
-        constraints.gridy = 0;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        panel.add(boton1, constraints);
+        JButton botonIniciar = new JButton("Iniciar");
+        metodoConstraints(constraints,3,1,2,1,1,1);
+        panel.add(botonIniciar,constraints);
 
-        // JButton en la fila 1, columna 2 y que ocupa 1x1 celdas.
-        JButton boton2 = new JButton ("Boton 2");
-        constraints.gridx = 2;
-        constraints.gridy = 1;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        panel.add(boton2, constraints);
+        JTextField etiquetaEscribirMensaje = new JTextField("escribe un mensaje");
+        metodoConstraints(constraints, 0,2,3,1,1,1);
+        panel.add(etiquetaEscribirMensaje,constraints);
 
-        // JButton en la fila 2, columna 0 y que ocupa 1x1 celdas.
-        JButton boton3 = new JButton ("Boton 3");
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        panel.add(boton3, constraints);
+        JButton botonEnviar = new JButton("Enviar");
+        metodoConstraints(constraints,3,2,2,1,1,1);
+        panel.add(botonEnviar,constraints);
 
-        // JButton en la fila 2, columna 2 y que ocupa 1x1 celdas.
-        JButton boton4 = new JButton ("Boton 4");
-        constraints.gridx = 2;
-        constraints.gridy = 2;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        panel.add(boton4, constraints);
+        JLabel etiquetaHexadecimal = new JLabel("mensajehexa");
+        metodoEtiqueta(etiquetaHexadecimal, Color.CYAN);
+        metodoConstraints(constraints, 0,3,4,1,1,1);
+        constraints.fill = GridBagConstraints.BOTH;
+        panel.add(etiquetaHexadecimal,constraints);
 
-        // JTextField en la fila 2, columna 1 y que ocupa 1x1 celdas.
-        JTextField campoTexto = new JTextField ("Campo texto");
-        constraints.gridx = 1;
-        constraints.gridy = 2;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        panel.add(campoTexto, constraints);
-
-
+        JLabel etiquetaMensaje = new JLabel("mensaje");
+        metodoEtiqueta(etiquetaMensaje, Color.CYAN);
+        metodoConstraints(constraints, 2,3,4,1,1,1);
+        constraints.fill = GridBagConstraints.BOTH;
+        panel.add(etiquetaMensaje,constraints);
 
         this.getContentPane().add(panel);
 
-//
-//        JLabel etiqueta = new JLabel("Servidor",SwingConstants.CENTER); //etiqueta texto y centrado
-////        etiqueta.setBounds(1,1,500,20); //tamaño y pocision
-//        etiqueta.setForeground(Color.cyan);//color del texto
-//        //etiqueta.setOpaque(true); //permitir modificar la etiqueta
-//        //etiqueta.setBackground(Color.cyan); //cambiar el fondo
-//
-//        GridBagConstraints cuadricula = new GridBagConstraints();
-//
-//        cuadricula.gridx = 0;
-//        cuadricula.gridy = 0;
-//        cuadricula.gridwidth = 2;
-//        cuadricula.gridheight = 1;
-//        cuadricula.weightx = 1;
-//        cuadricula.weighty = 1;
-//        cuadricula.fill = GridBagConstraints.HORIZONTAL;
-//
-//
-//        panel.add(etiqueta, cuadricula);//agregar etiqueta al panel
-//
-//        JLabel campoTexto = new JLabel("Prueba");
-//
-//////        campoTexto.setBounds(5,30,200,20);
-//
-//        cuadricula = new GridBagConstraints();
-//        cuadricula.gridx = 0;
-//        cuadricula.gridy = 1;
-//        cuadricula.gridwidth = 1;
-//        cuadricula.gridheight= 1;
-//        cuadricula.weightx = 1;
-//        cuadricula.weighty = 1;
-//        cuadricula.fill = GridBagConstraints.HORIZONTAL;
-//
-//        panel.add(campoTexto, cuadricula);
-//
-//        // Crear un botón
-//        JButton boton = new JButton("Obtener dato");
-//        boton.setBounds(5,50,10,10);
-//
-//        cuadricula = new GridBagConstraints();
-//        cuadricula.gridx = 0;
-//        cuadricula.gridy = 2;
-//        cuadricula.gridheight = 1;
-//        cuadricula.gridx = 1;
-//        cuadricula.gridwidth = 1;
-//        cuadricula.gridwidth = 1;
-//
-//        panel.add(boton, cuadricula);panel.add(campoTexto, cuadricula);
-//
-//        // Crear un botón
-//        JButton boton = new JButton("Obtener dato");
-//        boton.setBounds(5,50,10,10);
-//
-//        cuadricula = new GridBagConstraints();
-//        cuadricula.gridx = 0;
-//        cuadricula.gridy = 2;
-//        cuadricula.gridheight = 1;
-//        cuadricula.gridx = 1;
-//        cuadricula.gridwidth = 1;
-//        cuadricula.gridwidth = 1;
-//
-//        panel.add(boton, cuadricula);
-//
+
+    }
+    private GridBagConstraints metodoConstraints(GridBagConstraints constraints,
+                                           int x, int y,
+                                           int width, int height,
+                                           int weightx , int weighty)
+    {
+        //posicion
+        constraints.gridx = x;
+        constraints.gridy = y;
+        //Cuantas celdas abarca
+        constraints.gridwidth = width;
+        constraints.gridheight = height;
+        //Hacia donde se estira
+        constraints.weightx = weightx;
+        constraints.weighty = weighty;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+
+
+        return constraints;
+    }
+    private void metodoEtiqueta (JLabel etiqueta,Color color){
+        //etiqueta.setPreferredSize(new Dimension(width, height));
+        etiqueta.setOpaque(true); //permitir modificar la etiqueta
+        etiqueta.setBackground(color); //cambiar el fondo
+        return;
     }
 }
 
